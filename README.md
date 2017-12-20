28c77b67f115c11ced73b07ba3eb41514ae5b4a8
Ishan Subedi <ikushum@gmail.com>
2017-12-20 23:10:58 +0545

    mbf

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rails Starter Kit
 
-A bash script that makes it easy to install Ruby on Rails along with additional softwares and tools that everyone needs before getting started.
+A bash script that makes it easy to install Ruby on Rails along with additional must-have softwares development tools that everyone needs before getting started with ROR.
+
+Documentation comming soon :)
--- rsk.sh
+++ rsk.sh
@@ -12,58 +12,88 @@ fi
  
 if [[ $1 == 'rails' ]]
 	then
-
+	
 	sudo echo
+	echo -e '\e[93mWe strictly recommended you to install the underlined softwares. Only skip the installition if you have it pre installed on your system. \e[0m'
+	echo
 
-	read -p "Install Ruby ?* (y/n) : " rb
+	echo -e "Install \e[4mRuby\e[0m ? (\e[92my/\e[91mn\e[0m) : \c"
+	read rb
 	if [[ $rb == 'Y' || $rb == 'y' || $rb == '' ]]
 	then
 		rb=true
-		echo -e '\e[36mRuby will be installed\e[0m' >&2
+		echo -e '\e[36mRuby will be installed\e[0m' 
+		echo
 	else
 		rb=false
 		echo -e '\e[36mRuby will not be installed\e[0m'
+		echo
 	fi
 
-	read -p "Install Node.js ?* (y/n) : " njs
+	echo -e "Install \e[4mNode.js\e[0m ? (\e[92my/\e[91mn\e[0m) : \c"
+	read njs
 	if [[ $njs == 'Y' || $njs == 'y' || $njs == '' ]]
 	then
 		njs=true
-		echo -e '\e[36mNode.js will be installed\e[0m' >&2
+		echo -e '\e[36mNode.js will be installed\e[0m'
+		echo
 	else
 		njs=false
 		echo -e '\e[36mNode.js will not be installed\e[0m'
+		echo
 	fi	
 
-	read -p "Install Postgres ? (y/n) : " pg
+	echo -e "Install Postgresql ? (\e[92my/\e[91mn\e[0m) : \c"
+	read pg
 	if [[ $pg == 'Y' || $pg == 'y' || $pg == '' ]]
 	then
-		echo -e '\e[36mPostgres will be installed\e[0m'
+		echo -e '\e[36mPostgresql will be installed\e[0m'
+		echo
 		pg=true
 	else
 		pg=false
-		echo -e '\e[36mPostgres will not be installed\e[0m'
+		echo -e '\e[36mPostgresql will not be installed\e[0m'
+		echo
 	fi
 
-	read -p "Install Git ? (y/n) : " gt
+	echo -e "Install Git ? (\e[92my/\e[91mn\e[0m) : \c"
+	read gt
 	if [[ $gt == 'Y' || $gt == 'y' || $gt == '' ]]
 	then
 		echo -e '\e[36mGit will be installed\e[0m'
 		gt=true
+		echo	
 	else
 		gt=false
 		echo -e '\e[36mGit will not be installed\e[0m'
+		echo	
 	fi	
 
-	read -p "Install Heroku CLI ? (y/n) : " heroku
+	echo -e "Install Heroku CLI ? (\e[92my/\e[91mn\e[0m) : \c"
+	read heroku	
 	if [[ $heroku == 'Y' || $heroku == 'y' || $heroku == '' ]]
 	then
 		echo -e '\e[36mHeroku CLI will be installed\e[0m'
 		heroku=true
+		echo	
 	else
 		heroku=false
 		echo -e '\e[36mHeroku CLI will not be installed\e[0m'
-	fi		
+		echo	
+	fi	
+
+	echo -e "Install Sublime Text 3 ? (\e[92my/\e[91mn\e[0m) : \c"
+	read subl 
+	if [[ $subl == 'Y' || $subl == 'y' || $subl == '' ]]
+	then
+		echo -e '\e[36mSublime Text 3 will be installed\e[0m'
+		subl=true
+		echo	
+	else
+		subl=false
+		echo -e '\e[36mSublime Text 3 will not be installed\e[0m'
+		echo	
+	fi			
 
 	echo 
 	echo -e '\e[36mPreparing to Install...\e[0m'
@@ -103,7 +133,7 @@ if [[ $1 == 'rails' ]]
 
 	if $pg
 	then
-		echo -e '\e[36mInstalling Postgres...\e[0m'
+		echo -e '\e[36mInstalling Postgresql...\e[0m'
 		sudo apt-get install postgresql postgresql-contrib libpq-dev pgadmin3 -y
 	fi
 
@@ -118,6 +148,16 @@ if [[ $1 == 'rails' ]]
 		echo -e '\e[36mInstalling Heroku CLI...\e[0m'
 		wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 	fi
+
+	if $subl
+	then
+		echo -e '\e[36mInstalling Sublime Text 3...\e[0m'
+		sudo add-apt-repository ppa:webupd8team/sublime-text-3
+		sudo apt-get update
+		sudo apt-get install sublime-text-installer	
+	fi	
+
+	echo 
 
 elif [[ $1 == '' ]]
 then
